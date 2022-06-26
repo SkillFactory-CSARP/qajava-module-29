3b121c7de96576f740996ae33d5dcbdbb7308495
Ekaterina <katinarkhiff@gmail.com>
2022-06-26 21:23:27 +0300

    tests for Person + Person is also fixed

---
+++ src/main/java/Person.java
@@ -0,0 +1,13 @@
+public class Person {
+    public static boolean isTeenager(int age)
+    {
+        boolean result;
+        if (age <= 19 && age >= 13) {
+            result = true;
+        }
+        else {
+            result = false;
+        }
+        return result;
+    }
+}
