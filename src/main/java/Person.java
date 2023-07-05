277b258c1e94acc1a74e5ce1fcaa839150a0f49f
YuriiBazuev <132646060+YuriiBazuev@users.noreply.github.com>
2023-07-05 14:26:16 +0500

    new
    change old file

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,15 +1,28 @@
 public class Person {
     public static boolean isTeenager(int age) {
 
+        boolean result;
+        if (age < 19) {
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
 
-         return age >= 13 && age <= 19; }}
+//        public static boolean isTeenagers(int age) {
 //        boolean result;
-//        if (age < 19) {
+//        if (age >= 13 && age <= 19) {
 //            result = true;
 //        } else {
 //            result = false;
 //        }
 //        return result;
 //    }
-//}
+
+
+
+
+    }
+}
+
 
